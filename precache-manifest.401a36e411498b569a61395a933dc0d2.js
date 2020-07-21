f16f861829154a2f4c1fd5f148c58b14591753fd
Max Huang <letmaxknow@gmail.com>
2020-07-21 12:24:51 -0400

    Updates

---
+++ precache-manifest.401a36e411498b569a61395a933dc0d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59fe933357ce8aecf802e4ad8d6b3640",
+    "url": "/tumbleweed-go-frontend/index.html"
+  },
+  {
+    "revision": "0975f46087293d792059",
+    "url": "/tumbleweed-go-frontend/static/css/2.db529fa1.chunk.css"
+  },
+  {
+    "revision": "2e2ae5ded4bb7642ec62",
+    "url": "/tumbleweed-go-frontend/static/css/main.3e88b320.chunk.css"
+  },
+  {
+    "revision": "0975f46087293d792059",
+    "url": "/tumbleweed-go-frontend/static/js/2.a012c1df.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tumbleweed-go-frontend/static/js/2.a012c1df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e2ae5ded4bb7642ec62",
+    "url": "/tumbleweed-go-frontend/static/js/main.37634bee.chunk.js"
+  },
+  {
+    "revision": "9085372a0fa5ab4c9186",
+    "url": "/tumbleweed-go-frontend/static/js/runtime-main.8ff0f8d3.js"
+  }
+]);
